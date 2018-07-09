fe05a04e534e27147d0c40a6d0028caa12bda4bf
terminal625 <zpersoncool@gmail.com>
2018-07-09 03:21:23 -0400

    add lisp to c name

--- stuff.lisp
+++ stuff.lisp
@@ -2,3 +2,39 @@
   (:use #:cl #:utility))
 
 (in-package #:stuff)
+
+(defun copy-string (x)
+  (map 'string (function identity) x))
+
+(defparameter *buf* (make-array 0 :fill-pointer 0 :adjustable t :element-type 'character))
+(defun c-lisp-name (thing)
+  (setf (fill-pointer *buf*) 0)
+  (let ((str (string thing)))
+    )
+  (copy-string *buf*))
+(defun lisp-c-name (thing)
+  (setf (fill-pointer *buf*) 0)
+  (let* ((str (string thing))
+	 (len (length str)))
+    (if (zerop len)
+	(error "c names cannot be empty")
+	(let ((after-- nil)
+	      (char nil)
+	      (index 0))
+	  (tagbody
+	     start
+	     (setf char (aref str index))
+	     (let ((no-char (char-equal #\- char)))
+	       (unless no-char
+		 (if after--
+		     (vector-push-extend (char-upcase char)
+					 *buf*)
+		     (vector-push-extend (char-downcase char)
+					 *buf*)))
+	       (incf index)
+	       (when (>= index len)
+		 (go end))
+	       (setf after-- no-char))
+	     (go start)
+	     end))))
+  (copy-string *buf*))
